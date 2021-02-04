c4eae6d500a878b1d5f81ebd9835458e6e3a5eee
Wills Liou <willsliou@protonmail.com>
2021-02-03 22:21:14 -0800

    working doggo

--- read.py
+++ read.py
@@ -1,5 +1,12 @@
+# Installation
+# pip3 install opencv-contrib-python
+
 import cv2 as cv
 
 # Read-in image
 img = cv.imread('Photos/doggo.jpg')
 
+# Displays image as new window
+cv.imshow('Dog', img)
+# Wait for key press
+cv.waitKey(0)
